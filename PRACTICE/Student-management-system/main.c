38f421b2f40932e7e57a6a84a6c2af058527f38f
pshycodr <115141578+pshycodr@users.noreply.github.com>
2025-02-07 19:00:54 +0530

    Some code added

---
+++ PRACTICE/Student-management-system/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct Students
+{
+    char name[20];
+    int class;
+    int rollno;
+    char section;
+} Students;
+
+
+void addStudents()
+{
+    
+}
+int main()
+{
+
+    return 0;
+}
